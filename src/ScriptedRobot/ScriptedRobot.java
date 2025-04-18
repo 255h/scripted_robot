a109400588dec4ce57c53e6d47dd1a58630203f2
255h <255h@bk.ru>
2025-04-18 17:32:22 +0300

    readme

--- src/ScriptedRobot/ScriptedRobot.java
+++ src/ScriptedRobot/ScriptedRobot.java
@@ -8,6 +8,7 @@ public class ScriptedRobot {
 
     public static void main(String[] args) {
 
+
         String file_name = (args.length > 0)?args[0]:"useless_box.txt";
         Script script = ScriptFactory.GetScript(file_name);
         script.execute();
